e5ef266a5aea394b1e4ef2c528a09b7442567410
Jonathan Miller <millejon327@gmail.com>
2025-01-11 14:49:12 -0800

    Update band name generator

--- day-1/band_name_generator.py
+++ day-1/band_name_generator.py
@@ -1,11 +1,12 @@
-def generate_band_name() -> str:
-    """Return band name generated from user input."""
+def band_name_generator() -> None:
+    """Generate a band name."""
+    print("Welcome to the band name generator!")
+
     city = input("What city did you grow up in?\n")
     pet = input("What is your pet's name?\n")
 
-    return f"{city} {pet}"
+    print(f"Your band name could be {city} {pet}!")
 
 
 if __name__ == "__main__":
-    print("Welcome to the band name generator!")
-    print(f"Your band name could be {generate_band_name()}!")
+    band_name_generator()
