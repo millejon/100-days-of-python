435f637e552f33489dd29f4c66275dd11e71034e
Jonathan Miller <millejon327@gmail.com>
2025-01-11 20:52:24 -0800

    Update password generator

--- day-5/password_generator.py
+++ day-5/password_generator.py
@@ -6,13 +6,13 @@ NUMBERS = list(string.digits)
 SYMBOLS = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
 
 
-def generate_password(letters: int = 0, numbers: int = 0, symbols: int = 0) -> str:
-    """Return random combination of letters, numbers, and symbols.
+def compose_password(letters: int = 0, numbers: int = 0, symbols: int = 0) -> str:
+    """Compose a random password of letters, numbers, and symbols.
 
     Arguments:
-        letters -- Amount of letters to include in password
-        numbers -- Amount of numbers to include in password
-        symbols -- Amount of symbols to include in password
+        letters -- The amount of letters to include
+        numbers -- The amount of numbers to include
+        symbols -- The amount of symbols to include
 
     Returns:
         Randomly generated password
@@ -28,13 +28,19 @@ def generate_password(letters: int = 0, numbers: int = 0, symbols: int = 0) -> s
         password.append(random.choice(SYMBOLS))
 
     random.shuffle(password)
-
     return "".join(password)
 
 
-if __name__ == "__main__":
+def password_generator() -> None:
+    """Generate a random password."""
     print("Welcome to the password generator!")
+
     letters = int(input("How many letters would you like in your password?\n"))
     numbers = int(input("How many numbers would you like?\n"))
     symbols = int(input("How many symbols would you like?\n"))
-    print(f"Your password is {generate_password(letters, numbers, symbols)}")
+
+    print(f"Your password is {compose_password(letters, numbers, symbols)}")
+
+
+if __name__ == "__main__":
+    password_generator()
